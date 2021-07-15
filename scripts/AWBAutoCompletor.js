3d4cb440886dbc06b78c6e30cd2a1c2602cb523e
Nerl <andreas.nerl@web.de>
2021-07-15 09:40:37 +0200

    .

---
+++ scripts/AWBAutoCompletor.js
@@ -0,0 +1,70 @@
+//awbAutoCompletion = {
+class AwbAutoCompletor {
+
+    /** AwbAutoCompletor Class
+     * 
+     * 
+     * @param {string}       id   name/id the element of concern
+     * @param {string[][]}    idOfDataList name of the element datalist for the autocompletion
+     * 
+     */
+    constructor(dataArray, id, idOfDataList, callBackFunction) {
+        //this.dataArray = loadDataArray();
+        this.dataArray = dataArray;
+        this.id = id;
+        this.addOptionValues(idOfDataList);
+        this.addInputEventListener(id);
+        this.callBackFunction = callBackFunction;
+    }
+    /***
+     * 
+     */
+    getDataArray() {
+        return this.dataArray;
+    }
+
+
+    /****
+     * clear
+     */
+    clear() {
+        document.getElementById(this.id).value = "";
+    }
+    /**
+     * @private
+     */
+    addInputEventListener(elementID) {
+        document.getElementById(elementID).addEventListener('input', event => {
+            //console.log (this.dataArray);
+            var val = document.getElementById("input").value;
+            var opts = document.getElementById('datalist').childNodes;
+            for (var i = 0; i < opts.length; i++) {
+                if (opts[i].value === val) {
+                    // An item was selected from the list!
+                    // yourCallbackHere() cando 
+                    console.log(this.getDataArray()[i]);
+                    this.callBackFunction(this.getDataArray()[i]);
+                    //alert(this.dataArray[0].join());
+                    break;
+                }
+            }
+        });
+    };
+
+    /**
+     * @private
+     */
+    addOptionValues(idOfDataList) {
+        var optionValueElement;
+        this.dataArray.forEach(element => {
+            //console.log (element);
+            optionValueElement = document.createElement("option");
+            optionValueElement.value = element.join();
+            document.getElementById(idOfDataList).appendChild(optionValueElement);
+            //optionValueElement.text = dataArray[0].join();        
+        });
+    }
+}
+
+
+
