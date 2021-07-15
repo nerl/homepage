43f441c45f7abc8a7e3ed75bb9dcc581f810c387
Nerl <andreas.nerl@web.de>
2021-07-15 13:09:59 +0200

    Create date.js
    .

---
+++ scripts/date.js
@@ -0,0 +1,11 @@
+
+    function getFormattedDate() {
+        datum = new Date();
+        let month = "" + (datum.getMonth() + 1);
+        let day = "" + datum.getDate();
+        let year = datum.getFullYear();
+        if (month.length < 2) month = "0" + month;
+        if (day.length < 2) day = "0" + day;
+        let dateF = [day, month, year].join(".");
+        return dateF
+    }
