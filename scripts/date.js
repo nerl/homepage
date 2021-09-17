d646dddf8970e5030d04396136e3657c3b70ffbe
Nerl <andreas.nerl@web.de>
2021-09-17 17:32:38 +0200

    omeiomei

--- scripts/date.js
+++ scripts/date.js
@@ -10,3 +10,15 @@
         return dateF
     }
 
+
+    function getFormattedDateWithParameter(data) {
+        datum = new Date(data);
+        let month = "" + (datum.getMonth() + 1);
+        let day = "" + datum.getDate();
+        let year = datum.getFullYear();
+        if (month.length < 2) month = "0" + month;
+        if (day.length < 2) day = "0" + day;
+        let dateF = [day, month, year].join(".");
+        return dateF
+    }
+
